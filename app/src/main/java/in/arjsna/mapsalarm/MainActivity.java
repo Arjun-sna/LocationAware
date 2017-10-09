0b257e076dcaf14acdbfe3019b6ac2dc9440380b
Arjun <arjun@codebrahma.com>
2017-10-09 12:16:00 +0530

    set marker on current location

--- app/src/main/java/in/arjsna/mapsalarm/MainActivity.java
+++ app/src/main/java/in/arjsna/mapsalarm/MainActivity.java
@@ -1,25 +1,37 @@
 package in.arjsna.mapsalarm;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+// TODO: 9/10/17
+//<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
 public class MainActivity extends AppCompatActivity
     implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
-    GoogleMap.OnMyLocationClickListener {
+    GoogleMap.OnMyLocationClickListener, LocationListener {
 
   private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+  private static final long MIN_TIME = 400;
+  private static final float MIN_DISTANCE = 1000;
 
   private GoogleMap mMap;
   private boolean mPermissionDenied = false;
+  private LocationManager locationManager;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -27,6 +39,7 @@ public class MainActivity extends AppCompatActivity
     SupportMapFragment supportMapFragment =
         (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
     supportMapFragment.getMapAsync(this);
+    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
   }
 
   private void getPermissionAndEnableLocation() {
@@ -36,7 +49,16 @@ public class MainActivity extends AppCompatActivity
           Manifest.permission.ACCESS_FINE_LOCATION, true);
     } else if (mMap != null) {
       mMap.setMyLocationEnabled(true);
+      getCurrentLocation();
     }
+  }
+
+  private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+
+  @SuppressLint("MissingPermission")
+  private void getCurrentLocation() {
+    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE,
+        this);
   }
 
   @Override public void onMapReady(GoogleMap googleMap) {
@@ -78,5 +100,31 @@ public class MainActivity extends AppCompatActivity
   private void showMissingPermissionError() {
     PermissionUtils.PermissionDeniedDialog.newInstance(true)
         .show(getSupportFragmentManager(), "dialog");
+  }
+
+  @Override public void onLocationChanged(Location location) {
+    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+        new LatLng(location.getLatitude(), location.getLongitude()), 15), 10,
+        new GoogleMap.CancelableCallback() {
+          @Override public void onFinish() {
+
+          }
+
+          @Override public void onCancel() {
+
+          }
+        });
+  }
+
+  @Override public void onStatusChanged(String provider, int status, Bundle extras) {
+
+  }
+
+  @Override public void onProviderEnabled(String provider) {
+
+  }
+
+  @Override public void onProviderDisabled(String provider) {
+
   }
 }
