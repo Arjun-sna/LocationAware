922af43b1973ec3356dbb6842142b3d647abe8a6
Arjun <arjun@codebrahma.com>
2017-10-09 10:13:10 +0530

    show maps with location button

--- app/src/main/java/in/arjsna/mapsalarm/MainActivity.java
+++ app/src/main/java/in/arjsna/mapsalarm/MainActivity.java
@@ -1,12 +1,82 @@
 package in.arjsna.mapsalarm;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
+    GoogleMap.OnMyLocationClickListener {
+
+  private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+  private GoogleMap mMap;
+  private boolean mPermissionDenied = false;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    SupportMapFragment supportMapFragment =
+        (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+    supportMapFragment.getMapAsync(this);
+  }
+
+  private void getPermissionAndEnableLocation() {
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED) {
+      PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+          Manifest.permission.ACCESS_FINE_LOCATION, true);
+    } else if (mMap != null) {
+      mMap.setMyLocationEnabled(true);
+    }
+  }
+
+  @Override public void onMapReady(GoogleMap googleMap) {
+    mMap = googleMap;
+    mMap.setOnMyLocationButtonClickListener(this);
+    mMap.setOnMyLocationClickListener(this);
+    getPermissionAndEnableLocation();
+  }
+
+  @Override public boolean onMyLocationButtonClick() {
+    return false;
+  }
+
+  @Override public void onMyLocationClick(@NonNull Location location) {
+    Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+  }
+
+  @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+      @NonNull int[] grantResults) {
+    if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+      return;
+    }
+    if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+        Manifest.permission.ACCESS_FINE_LOCATION)) {
+      getPermissionAndEnableLocation();
+    } else {
+      mPermissionDenied = true;
+    }
+  }
+
+  @Override protected void onResumeFragments() {
+    super.onResumeFragments();
+    if (mPermissionDenied) {
+      showMissingPermissionError();
+      mPermissionDenied = false;
+    }
+  }
+
+  private void showMissingPermissionError() {
+    PermissionUtils.PermissionDeniedDialog.newInstance(true)
+        .show(getSupportFragmentManager(), "dialog");
   }
 }
